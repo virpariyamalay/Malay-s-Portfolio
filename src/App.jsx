f84ab0f62645033cbb99d22c4b21d2c3d6e6bb08
virpariyamalay <mpcvirpariya@gmail.com>
2025-05-10 23:11:56 +0530

    design updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    // <div className="min-h-screen bg-primary">
-    <div className="min-h-screen bg-white text-black dark:bg-primary dark:text-lightText transition-colors duration-300">
+    <div className="min-h-screen bg-primary">
+  
 
       <Navbar />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
