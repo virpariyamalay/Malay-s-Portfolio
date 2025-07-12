55a07b266a823d20ae17e2a0f7f39364c4dfc096
virpariyamalay <mpcvirpariya@gmail.com>
2025-07-12 18:12:59 +0530

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: `https://malay-s-portfolio.vercel.app/`,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 
 app.post('/api/contact', async (req, res) => {
